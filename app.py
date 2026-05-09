9fddeda249aaea6c962e64220d18da11aa56f6db
VEERESH KUMAR KALENJA <veereshkumarkalenja@VEERESHs-MacBook-Neo.local>
2026-05-09 10:22:09 +0530

    Pushing the 1st code to Git

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+quotes = [
+    "Keep learning DevOps",
+    "Automation saves time",
+    "Consistency creates mastery",
+    "Small progress every day"
+]
+
+@app.route("/")
+def home():
+    return {"message": "AI Quote Generator API Running"}
+
+@app.route("/quote")
+def quote():
+    return {
+        "quote": random.choice(quotes)
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
